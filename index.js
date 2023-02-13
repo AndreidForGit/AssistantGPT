7196d420275a636b44997db5557bd31342c97e12
Andreid <elequipobizarro@gmail.com>
2023-02-13 13:13:45 -0400

    delete log /start

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const bot = new Telegraf(envData.telegramToken);
 /////////BOT LOGIC////////
 bot.start((ctx) => {
     console.log('Message: ', ctx.message.text);
-    console.log('Message: ', envData);
     if (envData.dev.UsersId.includes(ctx.from.id) || envData.users.UsersId.includes(ctx.from.id)) {
         ctx.reply(`wellcome ${ctx.from.first_name}! \nThis is a playgroud for a chatbot like ChatGPT, `+
         'you can use OpenAI api by just writting anything.')
